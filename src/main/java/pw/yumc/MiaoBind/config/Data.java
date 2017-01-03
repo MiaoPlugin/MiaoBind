f716b2665d50f6a7a3e35df7a35c5117cab1a8e3
502647092 <admin@yumc.pw>
2017-01-03 20:40:50 +0800

    fix: 修复数据初始化问题
    Signed-off-by: 502647092 <admin@yumc.pw>

--- src/main/java/pw/yumc/MiaoBind/config/Data.java
+++ src/main/java/pw/yumc/MiaoBind/config/Data.java
@@ -5,13 +5,10 @@ import java.util.Map;
 
 import org.bukkit.inventory.ItemStack;
 
-import pw.yumc.YumCore.config.annotation.Nullable;
 import pw.yumc.YumCore.config.inject.InjectConfig;
 
 public class Data extends InjectConfig {
-    @Nullable
     public Map<String, List<ItemStack>> bindItems;
-    @Nullable
     public Map<String, List<ItemStack>> dropItems;
 
     public Data() {
